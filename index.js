a43bb45d86269be621f345d23ce170ce4192ad69
jiangcaiyang <jiangcaiyang12345@126.com>
2015-10-09 18:28:31 +0800

    Add some files
    These files are initially tested for availablity. And will be deleted afterwards.

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var Null = { };
+Null.onLoad = function ( params, callback )
+{
+	console.log( "----------------------------------------------------------------------" );
+	callback( );
+}
+
+module.exports = Null;
